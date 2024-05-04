b0ad713df1325bffd5b40f417cd366ac7810f964
Andreas Claesson <143418650+Mexus81@users.noreply.github.com>
2024-05-04 11:01:47 +0200

    ändrade färger

--- src/app/page.js
+++ src/app/page.js
@@ -2,17 +2,16 @@
 import { lazy } from 'react';
 
 const SearchLazy = lazy(() => import('./components/SearchArtist/Search'));
-import style from './pagelayout/data/page.module.css';
-import Search from './components/SearchArtist/Search'
+import styles from './pagelayout/data/page.module.css';
 
 let val =sessionStorage.getItem('LatestEvent');
 
 export default function Home() {
   return (
     <>
-      <div className={style.background}>
-      <p style={{color:"white"}}>Veckans tips:</p>
-      <p style={{color:"white"}}>{val}</p>
+      <div>
+      <p className={styles.pFont}>Veckans tips:</p>
+      <p className={styles.pFont}>{val}</p>
         <SearchLazy/>
       </div>
     </>
